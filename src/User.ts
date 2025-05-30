4438941e2d1250ef8668a5da3a90aae8151701b9
Sreyka <honsreyka6@gmail.com>
2025-05-30 14:27:53 +0700

    commit

--- src/User.ts
+++ src/User.ts
@@ -1,19 +1,1 @@
-export class User {
-    private user_id : string;
-    private name : string;
-    private email : string;
-    private address : string;
 
-    constructor(user_id: string, name: string, email: string, address: string) {
-        this.user_id = user_id;
-        this.name = name;
-        this.email = email;
-        this.address = address;
-    }
-
-    public logIn() : void{}
-    public logOut() : void{}
-    public register() : void{}
-    public changePassword() : void{}
-    public viewOrderHistory() : void{}
-}
