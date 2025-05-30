808d4b56ba3adb3b4ebb4bc0ed05c52f1649cb41
darinhoy <darin.hoy@student.passerellesnumeriques.org>
2025-05-30 15:45:34 +0700

    update method for  delivery

--- src/Main.ts
+++ src/Main.ts
@@ -1,8 +1,63 @@
 import { User } from "./User";
 import { Address } from "./Address";
+import { Delivery } from "./Delivery";
+import { DeliveryType } from "./DeliveryType";
 
 
 let user1 = new User("John Doe", "john@example.com", "09032423", new Address("123 Main St", "Cityville", "12345"), "password123");
 console.log(user1.register());
 console.log(user1.getUserInfo());
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Delivery
+const address = new Address("123 Main St", "Central", "Village A");
+
+const delivery = new Delivery(
+  1,
+  "TRACK123456",
+  "In Transit",
+  address,
+  DeliveryType.EXPRESS
+);
+console.log(address.getFullAddress())
+console.log(delivery.getDeliveryInfo());
