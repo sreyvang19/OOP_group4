3ebdf3530c060ecac1171898136525a767ee2c89
darinhoy <darin.hoy@student.passerellesnumeriques.org>
2025-06-01 13:12:28 +0700

    create the invoice

--- src/Main.ts
+++ src/Main.ts
@@ -4,7 +4,9 @@ import { Seller } from "./Seller";
 import { Delivery } from "./Delivery";
 import { DeliveryType } from "./DeliveryType";
 import { Product } from "./Product";
-
+import { OrderItem } from "./OrderItem";
+import { Order } from "./Order";
+import { Invoice } from "./Invoice";
 
 let user1 = new User("John Doe", "john@example.com", "09032423", new Address("123 Main St", "Cityville", "12345"), "password123");
 console.log(user1.register());
@@ -46,43 +48,36 @@ console.log(user1.getUserInfo());
 
 
 
+// Delivery
+const address = new Address("123 Main St", "Central", "Village A");
+
+const delivery = new Delivery(
+  1,
+  "TRACK123456",
+  "In Transit",
+  address,
+  DeliveryType.EXPRESS
+);
+console.log(address.getFullAddress())
+console.log(delivery.getDeliveryInfo());
 
 
+// --- Invoice Test ---
+const invoiceProduct = new Product("P100", "Monitor", 199.99, 30, 5);
+const invoiceOrderItem = new OrderItem(invoiceProduct, 2);
 
+const invoiceOrder = new Order(1001);
+invoiceOrder.addOrderItem(invoiceOrderItem);
 
+const invoice = new Invoice(
+  "INV-2025-001",
+  new Date(),
+  new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+  30,
+  invoiceOrder
+);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+invoice.generateInvoice();
 
 
 
@@ -138,15 +133,5 @@ product.adjustStock(-100);
 console.log(product.viewProduct());
 
 
-// Delivery
-const address = new Address("123 Main St", "Central", "Village A");
 
-const delivery = new Delivery(
-  1,
-  "TRACK123456",
-  "In Transit",
-  address,
-  DeliveryType.EXPRESS
-);
-console.log(address.getFullAddress())
-console.log(delivery.getDeliveryInfo());
+
